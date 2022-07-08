44da9a6bb7ea7a2ed382fef8e9b458496f201cec
cardinal <caren@merzhaus.org>
2022-07-08 19:08:58 -0400

    requires and connection

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const inquirer = require('inquirer');
 const mysql = require("mysql");
 const consoleTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3001,
+    user: "root",
+    password: "",
+    database: "tracker"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+});
