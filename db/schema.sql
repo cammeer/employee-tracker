c89a90c6298a7641f43bb1ebfad9da96af42e0fb
cardinal <caren@merzhaus.org>
2022-07-08 17:30:35 -0400

    set up schema

---
+++ db/schema.sql
@@ -0,0 +1,23 @@
+
+create table department (
+    id int not null auto_increment,
+    name varchar(30) not null,
+    primary key (id)
+);
+
+create table role (
+    id int not null auto_increment,
+    title varchar(30) not null,
+    salary decimal (10,2) not null,
+    department_id int not null,
+    primary key (id)
+);
+
+create table employee (
+    id int not null auto_increment,
+    first_name varchar(30) not null,
+    last_name varchar(30) not null,
+    role_id int not null,
+    manager_id int null,
+    primary key (id) 
+);
