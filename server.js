0d9913844855bdc0ddd2baa850d8e9263269a178
cardinal <caren@merzhaus.org>
2022-07-10 14:12:45 -0400

    fixed error yay

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
-// const mysql = require('mysql2');
-// const inquirer = require('inquirer');
-// const consoleTable = require('console.table');
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3306;
 
-// const db = mysql.createConnection({
-//         host: "localhost",
-//         port: 3001,
-//         user: "root",
-//         password: "",
-//         database: "tracker"
-//     },
-//     console.log('Connected to Tracker database.')
-// );
+const db = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "tracker"
+    },
+    console.log('Connected to Tracker database.')
+);
 
-// function start() {
-//     inquirer
-//         .prompt({
-//             type: "list",
-//             name: "option",
-//             message: "What would you like to do?",
-//             choices: [
-//                 "Add Department",
-//                 "Add Role",
-//                 "Add Employee",
-//                 "View Department",
-//                 "View Role",
-//                 "View Employee",
-//                 "Update Employee Role",
-//                 "Exit"
-//             ]
-//         })
-// }
+db.connect(function(err) {
+    if (err) throw err;
 
-// start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "option",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Update an employee role",
+                "Exit"
+            ]
+        })
+}
+
+start();
