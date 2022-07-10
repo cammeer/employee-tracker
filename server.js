11ab422bf9df3c4f07339cf6c2100a1e65569208
cardinal <caren@merzhaus.org>
2022-07-10 13:27:02 -0400

    deleted some files I'm not using

--- server.js
+++ server.js
@@ -1,13 +1,36 @@
-const mysql = require('mysql2');
-const inquirer = require('inquirer');
-const consoleTable = require('console.table');
+// const mysql = require('mysql2');
+// const inquirer = require('inquirer');
+// const consoleTable = require('console.table');
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 
-const db = mysql.createConnection({
-    host: "localhost",
-    port: 3001,
-    user: "root",
-    password: "",
-    database: "tracker"
-});
+// const db = mysql.createConnection({
+//         host: "localhost",
+//         port: 3001,
+//         user: "root",
+//         password: "",
+//         database: "tracker"
+//     },
+//     console.log('Connected to Tracker database.')
+// );
+
+// function start() {
+//     inquirer
+//         .prompt({
+//             type: "list",
+//             name: "option",
+//             message: "What would you like to do?",
+//             choices: [
+//                 "Add Department",
+//                 "Add Role",
+//                 "Add Employee",
+//                 "View Department",
+//                 "View Role",
+//                 "View Employee",
+//                 "Update Employee Role",
+//                 "Exit"
+//             ]
+//         })
+// }
+
+// start();
